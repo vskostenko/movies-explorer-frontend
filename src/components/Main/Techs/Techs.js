8bff5fd32d264c237f7dd68160162f47dcfac811
Vadim Kostenko <kostenko@mdrk.ru>
2023-07-21 16:27:45 +0700

    techs

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -9,9 +9,15 @@ function Techs (){
                 <hr className="article__line"></hr>
                 <h3 className="techs__header">7 технологий</h3>
                 <p className="techs__text" >На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-                <div>
-
-                </div>
+                <ul className="techs__list">
+                    <li className="techs__listitem">HTML</li>
+                    <li className="techs__listitem">CSS</li>
+                    <li className="techs__listitem">JS</li>
+                    <li className="techs__listitem">React</li>
+                    <li className="techs__listitem">Git</li>
+                    <li className="techs__listitem">Express.js</li>
+                    <li className="techs__listitem">mongoDB</li>
+                </ul>
             </div>
         </section>
     )
