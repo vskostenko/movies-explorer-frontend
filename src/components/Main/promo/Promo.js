756cc70b5657ca5267369664f1a14813740525ad
Vadim Kostenko <kostenko@mdrk.ru>
2023-07-28 13:12:09 +0700

    all resolution

--- src/components/Main/promo/Promo.js
+++ src/components/Main/promo/Promo.js
@@ -6,12 +6,10 @@ import "./Promo.css";
 function Promo() {
     return (
         <section className='promo'>
-            <div className="promo__container">
                 <h1 className='promo__title'>
                     Учебный проект студента факультета Веб-разработки.
                 </h1>
                 <div className='promo__img'/>
-            </div>
         </section>
     )
 }
