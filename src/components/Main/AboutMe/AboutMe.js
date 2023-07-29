6ca1443294028e7a2004f1f4961694780af93757
vskostenko <107306445+vskostenko@users.noreply.github.com>
2023-07-30 00:32:02 +0700

    make 404

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe () {
             </div>
             <div className="aboutme_container">
                 <div className="aboutme__textblock">
-                    <h2 className="aboutme__name">Виталий</h2>
+                    <h2 className="aboutme__name">Кермит</h2>
                     <h3 className="aboutme__prof">Фронтенд-разработчик, 30 лет</h3>
                     <p className="aboutme__bio">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
                     <a className='aboutme__git link' href='https://github.com/vskostenko' target='_blank' rel='noreferrer'>Github</a>
