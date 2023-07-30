557bedd37b9c98f70f1619ef744280a3287b2d71
vskostenko <107306445+vskostenko@users.noreply.github.com>
2023-07-30 20:33:39 +0700

    make review fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Helmet} from "react-helmet";
 import './index.css';
 import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <html lang="ru" />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
